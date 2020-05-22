70cf918b2128e7c9d72caac8dd603524d013b302
nitin <nshukla6@gmail.com>
2020-05-22 20:28:30 +0530

    production ready touch

--- src/api.js
+++ src/api.js
@@ -1,16 +1,27 @@
 import axios from 'axios';
 
 
+let githubClient;
+let githubSecret;
+
+if(process.env.NODE_ENV !== 'production') {
+  githubClient = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClient = process.env.GITHUB_CLIENT_ID;
+  githubSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 const URL = "https://api.github.com/users";
 const URL2 = "https://api.github.com/search/users?q=";
 export const getUser = async(searchKey) => {
   let url='';
   try{
-    url = searchKey ? `${URL2}${searchKey}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    url = searchKey ? `${URL2}${searchKey}&client_id=${githubClient}
+    &client_secret=${githubSecret}`
     :
-    `${URL}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
-    &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    `${URL}?client_id=${githubClient}
+    &client_secret=${githubSecret}`
     const users = await axios.get(url);
     return users;
   }
@@ -29,7 +40,8 @@ export const getProfile = async(id) => {
 }
 
 export const getRepos = async(login) => {
-  const url = `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc`;
+  const url = `https://api.github.com/users/${login}/repos?per_page=5&sort=created:asc
+  &client_id=${githubClient}&client_secret=${githubSecret}`;
   const repos = await axios.get(url);
   return repos;
 }
