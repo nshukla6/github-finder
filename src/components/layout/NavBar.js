bc26b04c9d3c0ba4cb457f59d5f4e2a8c64f5dbb
nitin <nshukla6@gmail.com>
2020-05-23 12:47:09 +0530

    change

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -10,7 +10,7 @@ const NavBar = ({icon, title}) => {
         </h1>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/github-finder">Home</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
